aeb199cf5670decd994746035c56ef1401d7d0ac
Nithin <nithinyell@gmail.com>
2020-04-25 23:07:13 +0530

    finished auth set up

--- Components/Login.js
+++ Components/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, ScrollView, TextInput, TouchableOpacity, Button } from 'react-native';
 import auth from '@react-native-firebase/auth';
 
@@ -6,37 +6,62 @@ import auth from '@react-native-firebase/auth';
 // https://github.com/nithinyell/react-native-firebase
 export default function Login() {
 
-    const [confirm, setConfirm] = useState(null);
-    const [code, setCode] = useState('');
+  const [confirm, setConfirm] = useState(false);
+  const [code, setCode] = useState('');
+  const [user, setUser] = useState(null);
+  const [userPhoneNumber, setUserphoneNumber] = useState(null)
 
-    async function signInWithPhoneNumber(phoneNumber) { 
-        console.log("**** SignIn Pressed")
-        const confirmation = await auth().signInWithPhoneNumber(phoneNumber)
-        setConfirm(confirmation);
+  async function signInWithPhoneNumber(phoneNumber) {
+    try {
+      const confirmation = await auth().signInWithPhoneNumber(phoneNumber)
+      setConfirm(confirmation);
+    } catch (error) {
+      console.log("error", error)
     }
+  }
 
-    async function confirmCode() {
-        try {
-          await confirm.confirm(code);
-          console.log("SuccessFull")
-        } catch (error) {
-          console.log('Invalid code.');
-        }
+  function onAuthStateChanged(user) {
+    if (user.displayName) {
+      setUser(ususer.displayNameer);
+    } else {
+      setUser(user.phoneNumber)
     }
+  }
 
-    if (!confirm) {
-        return (
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code);
+      const sub = auth().onAuthStateChanged(onAuthStateChanged)
+      console.log(sub)
+    } catch (error) {
+      console.log('Invalid code.', error);
+    }
+  }
+
+  if (user) {
+    return (
+      <View>
+        <Text>Hi {user}</Text>
+      </View>
+    )
+  } else {
+    if (confirm) {
+      return (
+        <>
+          <TextInput style={{ borderColor: 'red', borderRadius: 1, backgroundColor: 'red', width: 60 }} value={code} onChangeText={text => setCode(text)} />
+          <Button title="Confirm Code" onPress={() => confirmCode()} />
+        </>
+      )
+    } else {
+      return (
+        <View>
+          <TextInput onChangeText={text => setUserphoneNumber(text)} />
           <Button
             title="Phone Number Sign In"
-            onPress={() => signInWithPhoneNumber('+91 9618833467')}
+            onPress={() => signInWithPhoneNumber("+15413712599")}
           />
-        );
+        </View>
+      )
     }
-
-  return (
-    <>
-      <TextInput value={code} onChangeText={text => setCode(text)} />
-      <Button title="Confirm Code" onPress={() => confirmCode()} />
-    </>
-  );
+  }
 }
