41b77e503f4b2dc74feef043d57c3b1d671e980a
Nithin <nyallakara@ashleyfurniture.com>
2020-05-03 17:29:12 +0530

    check if you really requires 1. geolocation in package.json 2. jetifier in package.json 3. googlePlayServicesVersion = "16.0.0" &&& androidMapsUtilsVersion = "0.5+" in build gradle 4. buildConfigField("String", "RNGP_ANDROID_API_KEY", "\"${RNGP_ANDROID_API_KEY}\"") 5. implementation 'androidx.appcompat:appcompat:1.0.2'     implementation 'androidx.appcompat:appcompat:1.1.0-rc01'

--- Components/Ride/Request.js
+++ Components/Ride/Request.js
@@ -1,12 +1,20 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import MapView, {Marker} from 'react-native-maps'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import RNGooglePlaces from 'react-native-google-places';
+import Geolocation from '@react-native-community/geolocation';
 
 // https://aboutreact.com/react-native-map-example/
 
 export default function Request({ route, navigation }) {
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition((info) => {
+            //console.log(info)
+        })
+    })
 
     var mapStyle=[{"elementType": "geometry", "stylers": [{"color": "#242f3e"}]},{"elementType": "labels.text.fill","stylers": [{"color": "#746855"}]},{"elementType": "labels.text.stroke","stylers": [{"color": "#242f3e"}]},{"featureType": "administrative.locality","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#263c3f"}]},{"featureType": "poi.park","elementType": "labels.text.fill","stylers": [{"color": "#6b9a76"}]},{"featureType": "road","elementType": "geometry","stylers": [{"color": "#38414e"}]},{"featureType": "road","elementType": "geometry.stroke","stylers": [{"color": "#212a37"}]},{"featureType": "road","elementType": "labels.text.fill","stylers": [{"color": "#9ca5b3"}]},{"featureType": "road.highway","elementType": "geometry","stylers": [{"color": "#746855"}]},{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{"color": "#1f2835"}]},{"featureType": "road.highway","elementType": "labels.text.fill","stylers": [{"color": "#f3d19c"}]},{"featureType": "transit","elementType": "geometry","stylers": [{"color": "#2f3948"}]},{"featureType": "transit.station","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "water","elementType": "geometry","stylers": [{"color": "#17263c"}]},{"featureType": "water","elementType": "labels.text.fill","stylers": [{"color": "#515c6d"}]},{"featureType": "water","elementType": "labels.text.stroke","stylers": [{"color": "#17263c"}]}];        
 
