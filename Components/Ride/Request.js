00368cc01bb28379ceb64526a2105a593a661056
Nithin <nyallakara@ashleyfurniture.com>
2020-05-10 18:27:21 +0530

    set is done

--- Components/Ride/Request.js
+++ Components/Ride/Request.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, Button } from 'react-native';
 import MapView, {Marker} from 'react-native-maps'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import RNGooglePlaces from 'react-native-google-places';
 import Geolocation from '@react-native-community/geolocation';
 import MapViewDirections from 'react-native-maps-directions';
 import database from '@react-native-firebase/database';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 // https://aboutreact.com/react-native-map-example/
 
@@ -17,6 +18,17 @@ export default function Request({ route, navigation }) {
     const [currentLocation, setCurrentLocation] = useState(null)
     const [origin, setOrigin] = useState('')
     const [destination, setDestination] = useState('')
+
+    const [date, setDate] = useState(new Date());
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+    };
+
 
     var randomNumber = Math.floor(Math.random() * 100) + 1 
     let reference = '/Pool/' + randomNumber + '/'
@@ -43,14 +55,20 @@ export default function Request({ route, navigation }) {
             console.warn("ORIGIN AND DEST MUST BE SET")
     }
 
+    getMaxDate = () => {
+        var date = new Date()
+        date.setDate(date.getDate() + 14)
+        return date
+    }
+
     var mapStyle=[{"elementType": "geometry", "stylers": [{"color": "#242f3e"}]},{"elementType": "labels.text.fill","stylers": [{"color": "#746855"}]},{"elementType": "labels.text.stroke","stylers": [{"color": "#242f3e"}]},{"featureType": "administrative.locality","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#263c3f"}]},{"featureType": "poi.park","elementType": "labels.text.fill","stylers": [{"color": "#6b9a76"}]},{"featureType": "road","elementType": "geometry","stylers": [{"color": "#38414e"}]},{"featureType": "road","elementType": "geometry.stroke","stylers": [{"color": "#212a37"}]},{"featureType": "road","elementType": "labels.text.fill","stylers": [{"color": "#9ca5b3"}]},{"featureType": "road.highway","elementType": "geometry","stylers": [{"color": "#746855"}]},{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{"color": "#1f2835"}]},{"featureType": "road.highway","elementType": "labels.text.fill","stylers": [{"color": "#f3d19c"}]},{"featureType": "transit","elementType": "geometry","stylers": [{"color": "#2f3948"}]},{"featureType": "transit.station","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "water","elementType": "geometry","stylers": [{"color": "#17263c"}]},{"featureType": "water","elementType": "labels.text.fill","stylers": [{"color": "#515c6d"}]},{"featureType": "water","elementType": "labels.text.stroke","stylers": [{"color": "#17263c"}]}];        
     
     return (
         <View style={{ flex: 1, padding: 10 }}>
-            
+
             <View style={{ flex: 0.2, alignItems: 'center', }}>
                 <View style={{}}>
-                    <Text style={{fontWeight: 'bold'}}>Raise Request</Text>
+                    <Text style={{ fontWeight: 'bold' }}>Raise Request</Text>
                 </View>
 
                 <View style={{ flexDirection: 'row', marginTop: 15 }}>
@@ -64,9 +82,9 @@ export default function Request({ route, navigation }) {
                         }}>
                             <Text style={styles.header}>From:</Text>
                         </TouchableOpacity>
-                    <Text style={styles.subheader}>{origin != '' ? origin.name : 'Not Set'}</Text>
+                        <Text style={styles.subheader}>{origin != '' ? origin.name : 'Not Set'}</Text>
                     </View>
-                    <View style={{ flex: 0.5, backgroundColor: '#D98880'}}>
+                    <View style={{ flex: 0.5, backgroundColor: '#D98880' }}>
                         <TouchableOpacity onPress={() => {
                             RNGooglePlaces.openAutocompleteModal()
                                 .then((place) => {
@@ -76,17 +94,33 @@ export default function Request({ route, navigation }) {
                         }}>
                             <Text style={styles.header}>To:</Text>
                         </TouchableOpacity>
-                    <Text style={styles.subheader}>{destination != '' ? destination.name : 'Not Set'}</Text>
+                        <Text style={styles.subheader}>{destination != '' ? destination.name : 'Not Set'}</Text>
                     </View>
                 </View>
             </View>
-            <View style={{flex: 0.2, alignItems: 'center', justifyContent: 'center'}}>
+            <View style={{ flex: 0.2, alignItems: 'center', justifyContent: 'center' }}>
                 <TouchableOpacity onPress={() => sendData()}>
-                    <Text style={{fontWeight: '400', fontSize: 25}}>Confirm</Text>
+                    <Text style={{ fontWeight: '400', fontSize: 25 }}>Confirm</Text>
                 </TouchableOpacity>
             </View>
-            <View style={{ flex: 0.6 }}>
-                <MapView
+            <View style={{ flex: 0.3 }}>
+                <View>
+                    {(
+                        <DateTimePicker
+                        testID="dateTimePicker"
+                        minimumDate={new Date()}
+                        timeZoneOffsetInMinutes={0}
+                        value={date}
+                        mode='datetime'
+                        is24Hour={true}
+                        display="default"
+                        onChange={onChange}
+                      />
+                    )}
+                </View>
+            </View>
+            <View style={{ flex: 0.3 }}>
+                {/* <MapView
                     style={styles.map}
                     initialRegion={{
                         latitude: 37.78825,
@@ -96,8 +130,8 @@ export default function Request({ route, navigation }) {
                     }}
                     customMapStyle={mapStyle}
                 >
-                <MapViewDirections origin={origin.location} destination={destination.location} apikey={GOOGLE_MAPS_APIKEY}/>
-                </MapView>
+                    <MapViewDirections origin={origin.location} destination={destination.location} apikey={GOOGLE_MAPS_APIKEY} />
+                </MapView> */}
             </View>
         </View>
     )
